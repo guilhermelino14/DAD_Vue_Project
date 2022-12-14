db4acf322484ea8e45c64910afb91a1d26b80dab
Guilherme Lino <lino000000@hotmail.com>
2022-12-14 23:15:12 +0000

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -40,7 +40,7 @@ const AdminRoute = () => {
 
 const logout = () => {
   storeUser.logout();
-  cartStore.clearCart();
+  storeCart.clearCart();
   router.push('/');
   toast.success('Logged out with success');
 }
